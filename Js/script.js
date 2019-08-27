5a0f556d6e9c5f74b43ab31a1aed1e23b429edc8
Azghour-Saad <saad.azghour@gmail.com>
2019-08-27 23:30:51 +0100

    refactor navbar

--- Js/script.js
+++ Js/script.js
@@ -5,24 +5,29 @@
 
 const contactLink = document.querySelector('.Contact')
 const servicesLink = document.querySelector('.Services')
+const homepageLink = document.querySelector('.Homepage')
 const aboutLink = document.querySelector('.About')
 const ExternalLink = document.querySelector('.External')
 
 
 const contact = document.querySelector('.contact-form')
-const about = document.querySelector('.bg-cover')
+const homepage = document.querySelector('.bg-cover')
+const about = document.querySelector('.wrapper')
 const services = document.querySelector('.bg-services')
 const external = document.querySelector('.footer-content')
 
 
 const moveToContact = () => moveToElement(contact)
 const moveToAbout = () => moveToElement(about)
+const moveToHome = () => moveToElement(homepage)
 const moveToServices = () => moveToElement(services)
 const moveToFooter = () => moveToElement(external)
 
 
+
 contactLink.addEventListener("click", moveToContact)
 aboutLink.addEventListener("click", moveToAbout)
+homepageLink.addEventListener("click", moveToHome)
 servicesLink.addEventListener("click", moveToServices)
 ExternalLink.addEventListener("click", moveToFooter)
 
@@ -35,4 +40,25 @@ function moveToElement(elm) {
     })
 }
 
+
+
+const mainNav = document.getElementById('target-menu')
+const navToggle = document.getElementById('target-navbar')
+
+
+navToggle.addEventListener('click', function () {
+    // console.log(this.classList)
+    // console.log("Same this :", navToggle.classList)
+
+    if (this.classList.contains('active')) {
+        // console.log("yes", this.classList.contains('active'))
+        mainNav.style.display = "none"
+        this.classList.remove('active')
+    }
+    else {
+        // console.log("nooo", this.classList.contains('active'))
+        mainNav.style.display = "flex"
+        this.classList.add('active')
+    }
+})
 
